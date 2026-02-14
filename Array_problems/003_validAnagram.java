5066b67da64fcdf486fe2655b514d4e7c9c1a980
muralipartha2 <50797231+muralipartha2@users.noreply.github.com>
2026-02-14 17:14:01 +0530

    Update 003_validAnagram.java

--- Array_problems/003_validAnagram.java
+++ Array_problems/003_validAnagram.java
@@ -2,12 +2,14 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 		
-		int[] check_array= new int[26];
+		//check if the length of arr1 and arr2 are the same
 		if (s.length() != t.length())
 			return false;
-        
-		for(int i=0;i<s.length();i++){
 
+		//create an array with english alphabets as length
+        int[] check_array= new int[26];
+		//iterate through the first array
+		for(int i=0;i<s.length();i++){
             char c= s.charAt(i);
 			if(c >= 'a' && c<= 'z'){
 			check_array[c-'a']++;
@@ -15,7 +17,7 @@ class Solution {
 			check_array[c-'A']++;
 			}
 		}
-		
+		// iterate through the second array
 		for(int i=0;i<t.length();i++){
             char c= t.charAt(i);
 			if(c >= 'a' && c<= 'z'){
@@ -25,6 +27,7 @@ class Solution {
 			}
 		}
 
+		//check now whether the alphabet array is nuetralized to 0
         for(int i=0;i<check_array.length;i++){
             if(check_array[i] > 0)
             return false;
